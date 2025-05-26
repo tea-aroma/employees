28c660179f2f1c87b12b79407c87850e429ba487
tea-aroma <kovalyovictor71@gmail.com>
2025-05-26 17:40:24 +0500

    feat: Add the home controller and base routes.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Contracts\View\View;
+
+
+/**
+ * Provides logic for home actions.
+ */
+class HomeController extends Controller
+{
+    /**
+     * Shows the index page.
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        return view('admin.home.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([ 'prefix' => 'admin', 'as' => 'admin.' ], function ($group)
+{
+    $group->group([ 'prefix' => 'home', 'as' => 'home.' ], function ($group)
+    {
+        $group->get('/index', [ \App\Http\Controllers\HomeController::class, 'index' ])->name('index');
+    });
 });
