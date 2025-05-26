693a00e1502b7736c840a989adb58eda913f59af
tea-aroma <kovalyovictor71@gmail.com>
2025-05-26 17:39:53 +0500

    feat: Add base view and base assets files.

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Home Page')
+
+@section('content')
+
+    Forms..
+
+@endsection
---
+++ resources/views/admin/includes/header.blade.php
@@ -0,0 +1,11 @@
+<header class="header header--style-default">
+
+    <div class="header__column">
+        <svg width="15px" height="11px" class="header__icon">
+            <use xlink:href="{{ asset('assets/icons/sprite.svg') }}#user-location-fill"></use>
+        </svg>
+
+        <h1 class="header__heading">Employees</h1>
+    </div>
+
+</header>
